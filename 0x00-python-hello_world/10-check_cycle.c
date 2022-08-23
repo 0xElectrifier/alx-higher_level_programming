c96e78d1a6252569d2843c93bb62fe559b8ba7b6
Akacha Ubuntu <samchijioke16@gmail.com>
2022-08-23 19:36:11 +0100

    Fixed betty error

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,18 +10,18 @@
 size_t print_listint(const listint_t *h)
 {
 	const listint_t *current;
-    unsigned int n; /* number of nodes */
+	unsigned int n; /* number of nodes */
 
-    current = h;
-    n = 0;
-    while (current != NULL)
-    {
-	    printf("%i\n", current->n);
-	current = current->next;
-	n++;
-    }
+	current = h;
+	n = 0;
+	while (current != NULL)
+	{
+		printf("%i\n", current->n);
+		current = current->next;
+		n++;
+	}
 
-    return (n);
+	return (n);
 }
 
 /**
@@ -34,15 +34,15 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
 
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-	    return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	return (NULL);
 
-    new->n = n;
-    new->next = *head;
-    *head = new;
+	new->n = n;
+	new->next = *head;
+	*head = new;
 
-    return (new);
+	return (new);
 }
 
 /**
@@ -54,12 +54,12 @@ void free_listint(listint_t *head)
 {
 	listint_t *current;
 
-    while (head != NULL)
-    {
-	    current = head;
-	head = head->next;
-	free(current);
-    }
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
 }
 
 /**
