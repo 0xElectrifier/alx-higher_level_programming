f2be82fcdc9702eee792d7cf4f6877ddcc7875f0
Akacha Ubuntu <samchijioke16@gmail.com>
2022-08-23 19:17:45 +0100

    Modified 10-check* file

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -62,3 +62,29 @@ void free_listint(listint_t *head)
         free(current);
     }
 }
+
+/**
+ * check_cycle - function that checks if a listint_t list contains a cycle
+ * @list: pointer to ilst to be checked
+ *
+ * Return: 0 on success
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t **list_p, *copy;
+
+	if (list == NULL)
+		return (0);
+
+	list_p = &list;
+	copy = list->next;
+	while (copy != NULL)
+	{
+		if (*list_p == copy)
+			return (1);
+
+		copy = copy->next;
+	}
+
+	return (0);
+}
