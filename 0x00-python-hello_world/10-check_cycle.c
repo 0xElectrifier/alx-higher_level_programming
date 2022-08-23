52a092ab5427bc59ed484b9ce2961e6bec152913
Akacha Ubuntu <samchijioke16@gmail.com>
2022-08-23 19:30:32 +0100

    Fixed betty error

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
@@ -10,16 +9,16 @@
  */
 size_t print_listint(const listint_t *h)
 {
-    const listint_t *current;
+	const listint_t *current;
     unsigned int n; /* number of nodes */
 
     current = h;
     n = 0;
     while (current != NULL)
     {
-        printf("%i\n", current->n);
-        current = current->next;
-        n++;
+	    printf("%i\n", current->n);
+	current = current->next;
+	n++;
     }
 
     return (n);
@@ -33,11 +32,11 @@ size_t print_listint(const listint_t *h)
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-    listint_t *new;
+	listint_t *new;
 
     new = malloc(sizeof(listint_t));
     if (new == NULL)
-        return (NULL);
+	    return (NULL);
 
     new->n = n;
     new->next = *head;
@@ -53,13 +52,13 @@ listint_t *add_nodeint(listint_t **head, const int n)
  */
 void free_listint(listint_t *head)
 {
-    listint_t *current;
+	listint_t *current;
 
     while (head != NULL)
     {
-        current = head;
-        head = head->next;
-        free(current);
+	    current = head;
+	head = head->next;
+	free(current);
     }
 }
 
