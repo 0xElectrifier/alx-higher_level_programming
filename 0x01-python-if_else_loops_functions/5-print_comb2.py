07a62f0bbed8f5ae1a8be624f8ba5bcd6e3b37ca
Akacha Ubuntu <samchijioke16@gmail.com>
2022-08-27 16:11:21 +0100

    Passed end= as parameter

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,6 +3,6 @@
 
 for number in range(0, 100):
     if number == 99:
-        print("{:02}".format(number))
+        print("{:02d}".format(number))
     else:
-        print("{:02}, ".format(number), end="")
+        print("{:02d}".format(number), end=", ")
