fd345a3458fe64b86832313ac56389a709f6f0f4
Akacha Ubuntu <samchijioke16@gmail.com>
2022-08-27 20:30:10 +0100

    Rectified pycode error

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,6 @@ def uppercase(str):
     for c in str:
         charCode = ord(c)
         if charCode >= 97 and charCode <= 122:
-             c = chr(charCode - 32)
+            c = chr(charCode - 32)
         print("{}".format(c), end="")
     print("")
