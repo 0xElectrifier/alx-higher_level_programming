6a006b0a7d681dee9240c8655c5be0f33e882765
Akacha Ubuntu <samchijioke16@gmail.com>
2022-08-27 16:12:53 +0100

    T

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""Program that prints numbers from 0 to 99, separated by ", " """
-
 for number in range(0, 100):
     if number == 99:
-        print("{:02d}".format(number))
+        print("{}".format(number))
     else:
-        print("{:02d}".format(number), end=", ")
+        print("{:02}".format(number), end=", ")
