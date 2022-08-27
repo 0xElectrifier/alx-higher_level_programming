5692e052ddcabe1dcd26ba994daf482d6f49f3d1
Akacha Ubuntu <samchijioke16@gmail.com>
2022-08-27 16:05:29 +0100

    Reduced the print functions used from 3 to 2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,7 +2,7 @@
 """Program that prints numbers from 0 to 99, separated by ", " """
 
 for number in range(0, 100):
-    if number != 99:
-        print("{:02d}, ".format(number), end="")
+    if number == 99:
+        print("{:02d}".format(number))
     else:
-        print(f"{number}")
+        print("{:02d}, ".format(number), end="")
