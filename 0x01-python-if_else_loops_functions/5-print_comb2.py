0c02f73ec7da5e1d1bfb81c54f334b85b8113148
Akacha Ubuntu <samchijioke16@gmail.com>
2022-08-27 16:09:59 +0100

    Removed the 'd' format specifier

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,6 +3,6 @@
 
 for number in range(0, 100):
     if number == 99:
-        print("{:02d}".format(number))
+        print("{:02}".format(number))
     else:
-        print("{:02d}, ".format(number), end="")
+        print("{:02}, ".format(number), end="")
