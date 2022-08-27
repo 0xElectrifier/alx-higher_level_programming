2a57db980215edbe01c220ca645a48d38436285d
Akacha Ubuntu <samchijioke16@gmail.com>
2022-08-27 19:51:12 +0100

    Made function print newline on empty string argument passed

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def uppercase(str):
     length = len(str)
-    for i in range(length):
-        letter = str[i]
-        letterCode = ord(letter)
-        if letterCode >= 97 and letterCode <= 122:
-            letterCode -= 32
-            letter = chr(letterCode)
 
-        if i == (length - 1):
-            print("{}".format(letter))
-        else:
+    for i in range(length):
+        if length > 0:
+            letter = str[i]
+            letterCode = ord(letter)
+            if letterCode >= 97 and letterCode <= 122:
+                letterCode -= 32
+                letter = chr(letterCode)
             print("{}".format(letter), end="")
+
+    print("")
