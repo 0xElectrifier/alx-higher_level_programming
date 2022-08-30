d54297afa404f99e81ccedd92a0d518422d6d95f
Akacha Ubuntu <samchijioke16@gmail.com>
2022-08-30 20:19:11 +0100

    Corrected output string

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-
-"""Prints #pythoniscool using sys module"""
-__import__("os").write(1, "Hello World\n".encode("UTF-8"))
+__import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))
