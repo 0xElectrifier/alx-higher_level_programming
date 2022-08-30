afa580942469419261763f554cae135a5e15d4a1
Akacha Ubuntu <samchijioke16@gmail.com>
2022-08-30 16:12:37 +0100

    Made file non importable

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-"""Prints #pythoniscool using sys module"""
-from sys import stdout
+if __name__ == "__main__":
+    """Prints #pythoniscool using sys module"""
+    from sys import stdout
 
-stdout.write("#pythoniscool\n")
+    stdout.write("#pythoniscool\n")
