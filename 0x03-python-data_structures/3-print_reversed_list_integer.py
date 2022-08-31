3e34582cac20e65ebec06dd12deb2a2cd057e612
Akacha Ubuntu <samchijioke16@gmail.com>
2022-08-31 15:27:37 +0100

    Added :d format specifier

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,5 +3,5 @@
 def print_reversed_list_integer(my_list):
     """Prints all integers of a list, in reverse order"""
     if (my_list):
-        print("{}".format(my_list[len(my_list) - 1]))
+        print("{:d}".format(my_list[len(my_list) - 1]))
         print_reversed_list_integer(my_list[:-1])
