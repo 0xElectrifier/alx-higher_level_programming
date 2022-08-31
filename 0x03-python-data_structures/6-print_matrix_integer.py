da689db74d7aca8d438c1942f813312192523728
Akacha Ubuntu <samchijioke16@gmail.com>
2022-08-31 17:54:37 +0100

    Added :d format specifier

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,5 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         row_end = len(row) - 1
         for i in range(len(row)):
-            print("{}".format(row[i]), end=" " if i != row_end else "")
+            print("{:d}".format(row[i]), end=" " if i != row_end else "")
         print("")
