2c6f8963feae1b300da90f06f9b71ea3daacebd2
Akacha Ubuntu <samchijioke16@gmail.com>
2022-09-01 16:00:49 +0100

    Made code more efficient

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,25 +1,6 @@
 #include "lists.h"
 
 /**
- * list_len - checks for the length of a linked list
- * @head: pointer to head of list
- *
- * Return: length of iist
- */
-int list_len(listint_t *head)
-{
-	int len = 0;
-
-	while (head != NULL)
-	{
-		head = head->next;
-		len++;
-	}
-
-	return (len);
-}
-
-/**
  * is_palindrome - Function in C that checks if a singly linked list
  * 		is a palindrome
  * @head: pointer to to pointer to head of list
@@ -27,32 +8,30 @@ int list_len(listint_t *head)
  */
 int is_palindrome(listint_t **head)
 {
-	int len, i, j, back_idx;
-	listint_t *list_fwd, *list_bck;
+	listint_t *fast_ptr, *slow_ptr;
+	listint_t *firstHalf_ptr, *secondHalf_ptr;
 
 	if (!head || !*head)
 		return (1);
-
-	list_fwd = *head;
-	len = list_len(*head);
-	back_idx = len;
-	i = 0;
-
-	while (list_fwd && (i < len / 2))
+	fast_ptr = slow_ptr = firstHalf_ptr = *head;
+	while (fast_ptr != NULL && fast_ptr->next != NULL)
 	{
-		list_bck = *head;
-		j = 0;
-		while (list_bck && (j < back_idx - 1))
-		{
-			list_bck = list_bck->next;
-			j++;
-		}
-		if (list_bck && (list_fwd->n != list_bck->n))
-			return (0);
-		list_fwd = list_fwd->next;
-		back_idx--;
-		i++;
+		fast_ptr = fast_ptr->next->next;
+		slow_ptr = slow_ptr->next;
 	}
+	if (fast_ptr != NULL)
+		secondHalf_ptr = slow_ptr->next->next;
+	else
+		secondHalf_ptr = slow_ptr->next;
+	while (firstHalf_ptr && secondHalf_ptr)
+	{
+		if (firstHalf_ptr->n != secondHalf_ptr->n)
+			break;
+		firstHalf_ptr = firstHalf_ptr->next;
+		secondHalf_ptr = secondHalf_ptr->next;
+	}
+	if (secondHalf_ptr != NULL)
+		return (0);
 
 	return (1);
 }
