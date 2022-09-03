bbcece1b80bc21475ca0a8701502518b503f5bbe
Akacha Ubuntu <samchijioke16@gmail.com>
2022-09-03 15:25:52 +0100

    NUmber of keys

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
+
 def number_keys(a_dictionary):
-    num = 0
-    list_keys = list(a_dictionary.keys())
+    k = 0
+    for x in a_dictionary:
+        k += 1
 
-    for i in list_keys:
-        num += 1
-
-    return (num)
+    return (k)
