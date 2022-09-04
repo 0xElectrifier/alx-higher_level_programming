9eee97b15821a35695ad2d7e0bab1d8c5819f415
Akacha Ubuntu <samchijioke16@gmail.com>
2022-09-04 20:31:51 +0100

    Created function that prints a dictionary by ordered keys

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+
 def print_sorted_dictionary(a_dictionary):
-    list_ord = list(a_dictionary.keys())
-    list_ord.sort()
-    for i in list_ord:
-        print("{}: {}".format(i, a_dictionary.get(i)))
+    """Prints a dictionary by ordered keys"""
+    key_list = sorted(a_dictionary)
+    for item in key_list:
+        print("{}: {}".format(item, a_dictionary[item]))
