40391127dca162f1687e07536e64b7d71b4887cd
Akacha Ubuntu <samchijioke16@gmail.com>
2022-09-05 16:52:14 +0100

    Rectified pycodestyle error

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def complex_delete(a_dictionary, value):
+    """Deletes keys with a specific value in a dictionary"""
     prev_k = None
     for k, v in list(a_dictionary.items()):
         if v is value:
             del a_dictionary[k]
-
 
     return (a_dictionary)
