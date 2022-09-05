6e62eb77e1375ccfc90b7e2899b20396369da5ca
Akacha Ubuntu <samchijioke16@gmail.com>
2022-09-05 11:41:19 +0100

    Created function that converts Roman numerals within 1 - 3999 to integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,3 +4,30 @@ def roman_to_int(roman_string):
     if (roman_string is None) or (type(roman_string) is not str):
         return (0)
 
+    prev = roman_string[:1]
+    summ = 0
+    roman_dict = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+            }
+    prev = roman_dict.get(prev)
+    if prev is None:
+        prev = 0
+
+    for c in roman_string[1:]:
+        current = roman_dict.get(c)
+        if current is None:
+            current = 0
+        if prev < current:
+            prev *= -1
+        summ += prev
+        prev = current
+
+    summ += prev
+
+    return (summ)
