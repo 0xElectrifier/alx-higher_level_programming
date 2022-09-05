04c788fb1c4f52dbd783fb286152345d9c8bca88
Akacha Ubuntu <samchijioke16@gmail.com>
2022-09-05 14:10:20 +0100

    Created function that finds weight_average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
+
 def weight_average(my_list=[]):
-    if not my_list:
-        return 0
+    """Returns the weighted average of all integers tuple"""
+    if not list:
+        return (0)
 
-    num = 0
-    den = 0
+    weight_score = 0
+    weights = 0
+    for score, weight in my_list:
+        weight_score += (score * weight)
+        weights += weight
+    average = weight_score / weights
 
-    for tup in my_list:
-        num += tup[0] * tup[1]
-        den += tup[1]
-
-    return (num / den)
+    return (average)
