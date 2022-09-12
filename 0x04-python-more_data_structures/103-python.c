230339e8cbe4c0663fd1dcd927676dbef6edd1a8
Akacha Ubuntu <samchijioke16@gmail.com>
2022-09-12 19:04:34 +0100

    OBJ keeps hunting me

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -61,7 +61,7 @@ void print_python_list(PyObject *p)
 	size = PyList_Size(p);
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %ld\n", size);
-	printf("[*] Allocated = %ld\n", p->allocated);
+	printf("[*] Allocated = %ld\n", list->allocated);
 	for (i = 0; i < size; i++)
 	{
 		obj = list->ob_item[i];
