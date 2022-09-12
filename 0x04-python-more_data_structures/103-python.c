fe71e36ceb7e9e6bd1b8c83b2ce085db7a982822
Akacha Ubuntu <samchijioke16@gmail.com>
2022-09-12 04:44:00 +0100

    Solved problem?

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,4 +1,5 @@
-#include <>
+#include <byteobject.h>
+#include <string.h>
 
 /**
  * print_python_list - prints basic info about Python lists
@@ -8,10 +9,20 @@
  */
 void print_python_list(PyObject *p)
 {
-	printf("[*] Python list info\n\
-		[*] Size of the Python List = %HH\n\
-		[*] Allocated = %HH\n
-		")
+	PyListObject *obj;
+
+	if (!PyList_Check)
+		return;
+
+	obj = (PyListObject *)p;
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %HH\n", obj->ob_size);
+	printf("[*] Allocated = %HH\n", obj->allocated);
+	for (i = 0; i < list_len; i++)
+	{
+		printf("Element %d: \n", i);
+		printf("%s\n", ((obj->ob_item[i])->ob_type)->tp_name);
+	}
 }
 
 /**
@@ -22,7 +33,30 @@ void print_python_list(PyObject *p)
  */
 void print_python_bytes(PyObject *p)
 {
+	char *str;
+	int str_len, lim;
+
+	str = PyBytes_AsString(p);
+	str_len = strlen(str);
+	lim = str_len;
+	if (str_len > 10)
+		lim = 10;
+
 	printf("[.] bytes object info\n");
-	printf("size: %d\n", dd);
-	printf("first 6 bytes: %HH\n", dd);
+	if(!PyBytes_Check(p))
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
+	printf("size: %d\n", str_len);
+	printf("trying string: %s\n", str);
+	printf("first 6 bytes: ");
+	for (i = 0; i < lim; i++)
+	{
+		printf("%x");
+		if (i != (lim - 1))
+			printf(" ");
+		else
+			printf("\n");
+	}
 }
