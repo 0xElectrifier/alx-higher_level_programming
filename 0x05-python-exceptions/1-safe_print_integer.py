db6e13687ba24bb9dd7e0e6acf87d4eb9d1a4760
Akacha Ubuntu <samchijioke16@gmail.com>
2022-09-13 12:45:52 +0100

    Function that prints an integer with "{:d}".format() and returns false if argument is an integer. Solving problem using ValueError exception, without using type()

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
 
 def safe_print_integer(value):
-    """Print an integer with "{:d}".format().
-
-    Args:
-        value (int): The integer to print.
-
-    Returns:
-        If a TypeError or ValueError occurs - False.
-        Otherwise - True.
-    """
+    """Prints an integer with '{:d}'.format()"""
     try:
         print("{:d}".format(value))
-        return (True)
-    except (TypeError, ValueError):
+    except ValueError:
         return (False)
+    finally:
+        print("")
+        return (True)
