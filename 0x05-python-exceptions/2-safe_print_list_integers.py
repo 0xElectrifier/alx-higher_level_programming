3974a83593779278246d6a5357074409eca22d3d
Akacha Ubuntu <samchijioke16@gmail.com>
2022-09-13 14:54:13 +0100

    Handled IndexError and ValueError

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-    """Print the first x elements of a list that are integers.
+    """Prints the first 'x' elements of a list and only integers"""
+    count = 0
 
-    Args:
-        my_list (list): The list to print elements from.
-        x (int): The number of elements of my_list to print.
-
-    Returns:
-        The number of elements printed.
-    """
-    ret = 0
-    for i in range(0, x):
-        try:
+    for i in range(x):
+        try: 
             print("{:d}".format(my_list[i]), end="")
-            ret += 1
-        except (ValueError, TypeError):
-            continue
+        except (IndexError, ValueError):
+            pass
+        else:
+            count += 1
     print("")
-    return (ret)
+
+    return (count)
