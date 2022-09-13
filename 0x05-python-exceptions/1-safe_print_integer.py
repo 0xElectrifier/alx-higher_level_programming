aab75954194cdb62af305d7ddeb50adec3e2bef1
Akacha Ubuntu <samchijioke16@gmail.com>
2022-09-13 14:08:41 +0100

    Handled TypeError

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,7 +4,7 @@ def safe_print_integer(value):
     """Prints an integer with '{:d}'.format()"""
     try:
         print("{:d}".format(value))
-    except ValueError:
+    except (ValueError, TypeError):
         return (False)
     else:
         return (True)
