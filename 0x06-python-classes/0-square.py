279f1c2a2294b39ec6a57e1b2361b7b0cb2b77d5
Akacha Ubuntu <samchijioke16@gmail.com>
2022-09-16 21:07:11 +0100

    Added documentation to moduale

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+"""Module that demonstrates the creation of Class.
+"""
+
 class Square:
-    """An empty class to practice Class creating"""
+    """An empty class to practice Class creating."""
     pass
