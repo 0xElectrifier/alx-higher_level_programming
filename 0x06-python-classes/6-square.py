ec3e19b0309355cf973e31af5474a4489e415709
Akacha Ubuntu <samchijioke16@gmail.com>
2022-09-19 14:58:56 +0100

    Resolved minor errors

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -48,8 +48,8 @@ class Square:
         """
         if (type(value) is not tuple or
                 len(value) != 2 or
-                type(value[0]) is not int or
-                type(value[1]) is not int):
+                (not isinstance(value[0], int)) or (value[0] < 0) or
+                (not isinstance(value[1], int)) or (value[1] < 0)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
