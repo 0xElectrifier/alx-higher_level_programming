d43e3f86dabef59744ae7bfcc2aef4cdaf2ecd8c
Akacha Ubuntu <samchijioke16@gmail.com>
2022-09-19 14:25:51 +0100

    Added new attribute, position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,60 +1,68 @@
 #!/usr/bin/python3
 
-"""Define a class Square."""
+"""Creates a class Square"""
 
 
 class Square:
-    """Represent a square."""
+    """Defines a square based on '5-square.py'"""
 
     def __init__(self, size=0, position=(0, 0)):
-        """Initialize a new square.
+        """Initializes the square
 
         Args:
-            size (int): The size of the new square.
-            position (int, int): The position of the new square.
+            size (int): size of the square
+            position (int): f
         """
         self.size = size
         self.position = position
 
     @property
     def size(self):
-        """Get/set the current size of the square."""
+        """Getter method for self.size"""
         return (self.__size)
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
+        """Setter method for self.size
+
+        Args:
+            value (int): size of square
+        """
+        if (type(value) is not int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if (value < 0):
             raise ValueError("size must be >= 0")
         self.__size = value
 
     @property
     def position(self):
-        """Get/set the current position of the square."""
+        """Getter method for self.size"""
         return (self.__position)
 
     @position.setter
     def position(self, value):
-        if (not isinstance(value, tuple) or
+        """Setter method for self.position
+
+        Args:
+            value (int): size of square
+        """
+        if (type(value) is not tuple or
                 len(value) != 2 or
-                not all(isinstance(num, int) for num in value) or
-                not all(num >= 0 for num in value)):
+                type(value[0]) is not int or
+                type(value[1]) is not int):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
-        """Return the current area of the square."""
-        return (self.__size * self.__size)
+        """Calculates area of the square"""
+        return (self.__size ** 2)
 
     def my_print(self):
-        """Print the square with the # character."""
-        if self.__size == 0:
-            print("")
-            return
-
-        [print("") for i in range(0, self.__position[1])]
-        for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
-            print("")
+        """Prints out the square with the character '#'"""
+        [print("") for i in range(self.position[1])]
+        for i in range(self.size):
+            [print(" ", end="") for j in range(self.position[0])]
+            [print("#", end="") for j in range(self.size)] 
+            if (i != range(self.size)[-1]):
+                print("")
+        print("")
