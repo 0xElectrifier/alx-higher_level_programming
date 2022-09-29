b7380f2c146482aaa04e62e66dd148b432c59eb1
Akacha Ubuntu <samchijioke16@gmail.com>
2022-09-30 00:49:20 +0100

    Wrote custom __str__ method for square

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
-"""Defines a Rectangle subclass Square."""
-Rectangle = __import__('9-rectangle').Rectangle
+
+"""Creates a class Square"""
+Rectangle = __import__("9-rectangle").Rectangle
 
 
 class Square(Rectangle):
-    """Represent a square."""
+    """Defines an object which is a subclass of `Rectangle`"""
 
     def __init__(self, size):
-        """Initialize a new square.
+        """Initializes the square.
 
         Args:
-            size (int): The size of the new square.
+            size (int): size of square.
         """
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
+    def __str__(self):
+        """Returns the string representation of the object"""
+        return ("[Square] {}/{}".format(self.__size, self.__size))
