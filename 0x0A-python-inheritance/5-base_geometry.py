838cc18bb6b8aab3382a4606cf348436b9aeeea9
Akacha Ubuntu <samchijioke16@gmail.com>
2022-09-29 15:27:37 +0100

    Created empty object

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""Defines an empty class BaseGeometry."""
+
+"""Defines class BaseGeometry"""
 
 
 class BaseGeometry:
-    """Represent base geometry."""
+    """Defines an empty instance of type BaseGeometry"""
+
     pass
