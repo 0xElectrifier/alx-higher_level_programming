5c6de89270a5e7353a11c842bef3532321fb24d3
Akacha Ubuntu <samchijioke16@gmail.com>
2022-09-29 10:37:20 +0100

    Created a subclass of list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""Defines an inherited list class MyList."""
+
+"""Defines a class MyList"""
 
 
 class MyList(list):
-    """Implements sorted printing for the built-in list class."""
+    """Creates an improved list object"""
 
     def print_sorted(self):
-        """Print a list in sorted ascending order."""
-        print(sorted(self))
+        """Prints element of the list in sorted ascending order"""
+        sorted_list = sorted(self)
+        print(sorted_list)
