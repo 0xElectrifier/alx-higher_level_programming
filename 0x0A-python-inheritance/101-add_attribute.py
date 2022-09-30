f25158bfafae37f2b2cf540bfb01429289e245e5
Akacha Ubuntu <samchijioke16@gmail.com>
2022-09-30 02:23:37 +0100

    Appended function documentation

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -3,16 +3,16 @@
 """Defines a function, add_attribute"""
 
 
-def add_attribute(self, var, value):
-    """This function is equivalent to self.var = value
+def add_attribute(obj, var, value):
+    """This function is equivalent to obj.var = value
 
     Args:
+        obj (object): object to be modified
         var (any): the variable to be added as attribute
-        value (any): the value to be assigned to the variables
-
+        value (any): the value to be assigned to the variables.
     Raises:
         TypeError: if attribute can't be created
     """
     if not hasattr(self, "__dict__"):
         raise TypeError("can't add new attributes")
-    setattr(self, var, value)
+    setattr(obj, var, value)
