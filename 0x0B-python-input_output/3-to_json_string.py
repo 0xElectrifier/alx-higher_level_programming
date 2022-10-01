a871d77f344525df0a86eda223f17f6b0570dedb
Akacha Ubuntu <samchijioke16@gmail.com>
2022-10-01 13:37:57 +0100

    Created a function that serializes a Python object

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-"""Defines a string-to-JSON function."""
+"""Defines a function `to_js0n_string`"""
+
+
 import json
-
-
 def to_json_string(my_obj):
-    """Return the JSON representation of a string object."""
-    return json.dumps(my_obj)
+    """Returns the JSON representation of @my_obj
+
+    Args:
+        my_obj (object): object to be used
+    """
+    return (json.dumps(my_obj))
