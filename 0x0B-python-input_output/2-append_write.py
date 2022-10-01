13ffdf87a6020760b14dc2d5d05a526ce66ae13a
Akacha Ubuntu <samchijioke16@gmail.com>
2022-10-01 12:38:10 +0100

    Wrote a functin that appends to a file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
-"""Defines a file-appending function."""
+
+"""Defines a funtion `append_write`"""
 
 
 def append_write(filename="", text=""):
-    """Appends a string to the end of a UTF8 text file.
+    """Appends @text to the end of @filename
 
     Args:
-        filename (str): The name of the file to append to.
-        text (str): The string to append to the file.
+        filename (str): file to be editted
+        text (str): text to be appended into @filename
+    
     Returns:
-        The number of characters appended.
+        The number of characters added
     """
-    with open(filename, "a", encoding="utf-8") as f:
-        return f.write(text)
+    try:
+        with open(filename, "x", encoding="utf-8") as f:
+            ret = f.write(text)
+    except FileExistsError:
+        with open(filename, "a", encoding="utf-8") as f:
+            ret = f.write(text)
+
+    return (ret)
