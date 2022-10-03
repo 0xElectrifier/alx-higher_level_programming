066eed8d7b0f216068a18ffec26f2f400f1e4081
Akacha Ubuntu <samchijioke16@gmail.com>
2022-10-03 15:43:03 +0100

    Wrote a function that appends texts into a file

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
-"""Defines a text file insertion function."""
+"""Defines a function ``append_after``"""
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """Insert text after each line containing a given string in a file.
+    """Inserts a linew of text to a file, after each line containing
+    a specific string
 
     Args:
-        filename (str): The name of the file.
-        search_string (str): The string to search for within the file.
-        new_string (str): The string to insert.
+        filename (str): name of file
+        search_string (str): string to be searched for
+        new_string (str): string to be replaced
     """
     text = ""
-    with open(filename) as r:
-        for line in r:
+    with open(filename, "r", encoding="utf-8") as f:
+        line = f.readline()
+        while (line):
             text += line
             if search_string in line:
                 text += new_string
-    with open(filename, "w") as w:
-        w.write(text)
+            line = f.readline()
+
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(text)
