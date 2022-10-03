7981442c4b4f3094701c83c52724fe213ffd540f
Akacha Ubuntu <samchijioke16@gmail.com>
2022-10-03 20:23:44 +0100

    Initializing project

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""First Project"""
+
+
+class Base:
+    """Base class of all other classes in this project. It manages
+    ``id`` attribute in all other classes, to avoid duplicating the
+    same code"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initializes Object
+
+        Args:
+            id (int):
+        """
+        if (id is not None):
+            __nb_objects += 1
+            self.id = id
