4684f47e5bc1f41fa7043e29c64b2eb1d26cd8ae
Akacha Ubuntu <samchijioke16@gmail.com>
2022-10-04 23:04:53 +0100

    More genius work

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,7 @@
 import json
 
 
+
 class Base:
     """Base class of all other classes in this project. It manages
     ``id`` attribute in all other classes, to avoid duplicating the
@@ -35,3 +36,25 @@ class Base:
         if ((list_dictionaries is []) or (list_dictionaries is None)):
             return ("[]")
         return (json.dumps(ld))
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Class method that writes to a JSON file the json representation
+        of list_objs
+
+        Args:
+            list_objs (obj): list of instances who inherits of ``Base`` class
+        """
+        filename = cls.__name__ + ".json"
+        if (list_objs is None):
+            with open(filename, "w", encoding="utf-8") as f:
+                f.write("")
+        else:
+            j_list = []
+            for obj in list_objs:
+                b_dict = obj.to_dictionary()
+                j_list.append(b_dict)
+
+            j_str = Base.to_json_string(j_list)
+            with open(filename, "w", encoding="utf-8") as f:
+                f.write(j_str)
