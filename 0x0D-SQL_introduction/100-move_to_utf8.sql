c1b2d3d38266261493df05906f46d6c9cabeaad5
Akacha-Ubuntu <samchijioke16@gmail.com>
2022-11-04 14:34:45 +0100

    Quick fix

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,6 @@
 -- Converts 'hbtn_0c_0' database to UTF-8 
 -- Source https://stackoverflow.com/questions/72715542/character-set-vs-convert-to-character-set
+USE hbtn_0c_0;
 ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
