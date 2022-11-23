1885bbfc468d1ff23a16c95e587184661ed53a91
Electrifier-Ubuntu <samchijioke16@gmail.com>
2022-11-23 22:55:41 +0100

    Using `ORDER BY`

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,5 @@
 -- Lists all records of 'second_table' of the 'hbtn_0c_0' database in your
 -- MySQL server.
-SELECT score, name
-FROM second_table
-ORDER BY score DESC;
+SELECT `score`, `name`
+  FROM `second_table`
+  ORDER BY `score` DESC;
