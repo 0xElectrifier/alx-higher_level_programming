0fe93763db29807ae295ae59c1d47a33477d6912
Electrifier-Ubuntu <samchijioke16@gmail.com>
2022-11-24 20:54:32 +0100

    New updates

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,7 @@
 -- Converts 'hbtn_0c_0' database to UTF-8 
 -- Source https://stackoverflow.com/questions/72715542/character-set-vs-convert-to-character-set
-USE hbtn_0c_0;
-ALTER TABLE first_table
-CONVERT TO CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
+USE `hbtn_0c_0`
+AL
+ALTER TABLE `first_table`
+  CONVERT TO CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,7 @@
+-- Imports table in 'temperature.sql' file and,
+-- Displays the average temperature (Fahrenheit) by city ordered by
+-- temperature (descending)
+SELECT `city`, AVG(value) as avg_temp
+FROM `temperatures`
+GROUP BY `city`
+ORDER BY avg_temp DESC;
