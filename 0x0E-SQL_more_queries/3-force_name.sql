bd0140aff2f959fca1617c45faf4e51a92beb2a7
Electrifier-Ubuntu <samchijioke16@gmail.com>
2022-11-27 22:50:24 +0100

    Reattempted task

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,5 +1,5 @@
--- create a table with specific columns
+-- Creates the table 'force_name' on your MySQL server
 CREATE TABLE IF NOT EXISTS force_name (
-  id INT,
-  name VARCHAR(256) NOT NULL
-  );
+       `id` INT,
+	`name` VARCHAR(256) NOT NULL
+)
