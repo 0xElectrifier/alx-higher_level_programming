c52ba9ef5af81b3b4a4427317a6eebfe4dc75960
Electrifier-Ubuntu <samchijioke16@gmail.com>
2022-11-27 16:16:18 +0100

    Script that creates a mysql server user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,6 @@
--- create user and set password
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+-- Creates the MySQL server user 'user_0d_1'
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+   IDENTIFIED BY 'user_0d_1_pwD';
+GRANT ALL PRIVILEGES
+   ON *.*
+   TO 'user_0d_1'@'localhost';
