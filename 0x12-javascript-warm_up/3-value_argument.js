2cd959dcd08c3327e6be8fddfa4903b095f21a7e
Electrifier-Ubuntu <samchijioke16@gmail.com>
2022-11-30 10:56:28 +0100

    Resolved semistandard error, 'Unexpected tab character'

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const argv = process.argv;
 if (argv[2] === undefined) {
-	console.log('No argument');
-	process.exit(1);
+  console.log('No argument');
+  process.exit(1);
 }
 console.log(argv[2]);
