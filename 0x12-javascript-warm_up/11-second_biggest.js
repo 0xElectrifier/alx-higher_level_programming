4adc5e404f3003b690a638634aaf1194116a49b2
Electrifier-Ubuntu <samchijioke16@gmail.com>
2022-11-30 21:15:40 +0100

    Second largest number in an array of arguments

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+const argc = process.argv.length - 2;
+const argv = process.argv;
+let current, max, secondMax;
+if (argc == 0 || argc == 1) {
+  secondMax = 0;
+} 
+ 
+if (argv[3] > argv[2]) {
+  max = argv[3];
+  secondMax = argv[2];
+} else {
+  max = argv[2];
+  secondMax = argv[3];
+}
+max = parseInt(max);
+secondMax = parseInt(max);
+for (let i = 4; i < (argc + 2); i++) {
+  current = parseInt(argv[i]);
+  if (current > max) {
+    if (secondMax < max) {
+      secondMax = max;
+      max = current;
+    }
+  } else if (current < max) {
+    if (current > secondMax) {
+      seondMax = current;
+    }
+  }
+}
+
+console.log(secondMax);
