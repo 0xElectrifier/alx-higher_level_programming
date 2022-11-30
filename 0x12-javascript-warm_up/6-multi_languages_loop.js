57b77340eeb9d19f294f290853ff91733ac6fdcf
Electrifier-Ubuntu <samchijioke16@gmail.com>
2022-11-30 12:52:41 +0100

    Used a loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,3 +1,5 @@
 #!/usr/bin/node
 arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-arr.forEach(value => console.log(value));
+for (let i = 0; i < arr.length; i++) {
+	console.log(arr[i]);
+}
