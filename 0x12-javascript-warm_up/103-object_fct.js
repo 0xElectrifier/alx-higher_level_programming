adb71c7d9494dde5b4ba98a422dfa779d83aa5e5
Electrifier-Ubuntu <samchijioke16@gmail.com>
2022-12-01 12:31:05 +0100

    More func

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12,
+  incr: ()=> {
+  myObject.value++;
+}
+};
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
