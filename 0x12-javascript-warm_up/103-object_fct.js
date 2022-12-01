dc4879e63ce2dc647df8b6f6c5895d5ecae233ff
Electrifier-Ubuntu <samchijioke16@gmail.com>
2022-12-01 12:38:31 +0100

    Resolved semistandard errors

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -2,9 +2,9 @@
 const myObject = {
   type: 'object',
   value: 12,
-  incr: ()=> {
-  myObject.value++;
-}
+  incr: () => {
+    myObject.value++;
+  }
 };
 console.log(myObject);
 myObject.incr();
