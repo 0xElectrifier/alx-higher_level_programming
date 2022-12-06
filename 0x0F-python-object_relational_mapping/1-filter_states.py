0bb6d5d92e06567ca7094efb0c6e566c65fc0d16
Akacha-Ws <samchijioke16@gmai.com>
2022-12-06 09:58:47 -0800

    Added an if statement to handle, 'N'

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,8 @@ if __name__ == '__main__':
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
     query_rows = cur.fetchall()
     for row in query_rows:
-        print(row)
+        if row[1][0] == 'N':
+            print(row)
 
     cur.close()
     conn.close()
