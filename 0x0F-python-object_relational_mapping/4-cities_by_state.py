937ec9e8afecf8af50ae375684ae1ea731a48400
Akacha-Ws <samchijioke16@gmai.com>
2022-12-06 14:44:47 -0800

    Rectified code not executing, changed '__name__' option to '__main__'

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -4,11 +4,13 @@ import MySQLdb
 from sys import argv
 
 
-if __name__ == '__name__':
+if __name__ == '__main__':
     conn = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.exectute("SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id")
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
+                 INNER JOIN states ON cities.state_id=states.id\
+                 ORDER BY cities.id")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
