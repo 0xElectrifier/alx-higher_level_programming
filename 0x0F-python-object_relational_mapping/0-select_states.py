7e23ce6b0755ef5313a1d0c4e105e34a2aa6ecb9
Akacha-Ws <samchijioke16@gmai.com>
2022-12-06 03:17:59 -0800

    Changed 127.* to 'localhost'

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """Script that lists all states from the database `hbtn_0e_0_usa`"""
+import MySQLdb
+import sys
 
 
 if __name__ == '__main__':
-    import MySQLdb
-    import sys
-
     argv = sys.argv
-    db = MySQLdb.connect(host='127.0.0.1', user=argv[1], passwd=argv[2],
+    db = MySQLdb.connect(host='localhost', user=argv[1], passwd=argv[2],
                          db=argv[3], port=3306)
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
