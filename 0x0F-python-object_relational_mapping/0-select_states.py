1112e7e33276d549bccc3125c487cb8d557db9ee
Akacha-Ws <samchijioke16@gmai.com>
2022-12-06 02:47:48 -0800

    Tested file offline, works good

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,6 +6,11 @@ import sys
 """Script that lists all states from the database `hbtn_0e_0_usa`"""
 if __name__ == '__main__':
     argv = sys.argv
-    db = MySQL.connect(host='127.0.0.1:3306', user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host='127.0.0.1', user=argv[1], passwd=argv[2], db=argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
