d8d4ca9bd64d0a57e01ce94bf51e01b84c26136e
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2022-12-16 01:18:23 +0100

    Used 'cut' command to make the output more concise

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,4 @@
 # Script that takes in a URL, sends a request to that URL, and displays
 # the size of the body of the response
 
-curl -s -i $1 | grep 'Content-Length'
+curl -s -i $1 | grep 'Content-Length' | cut -d " " -f2
