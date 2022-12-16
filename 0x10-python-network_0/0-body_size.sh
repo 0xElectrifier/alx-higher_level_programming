fc4b5f0dad253c750514bdd9d4307fc0532b70ba
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2022-12-16 01:21:20 +0100

    Made my file 3 lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Script that takes in a URL, sends a request to that URL, and displays
-# the size of the body of the response
-
+# Takes in a URL and displays the size of the body of the response using curl
 curl -s -i $1 | grep 'Content-Length' | cut -d " " -f2
