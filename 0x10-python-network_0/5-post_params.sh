db06f9fdb4247bc143aefa3b8ca2cf7846e87a8c
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2022-12-16 15:57:57 +0100

    First post command using curl

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# display methods
-curl -s -X "POST" -d "email=test@gmail.com&subject=I will always be here for PLD" $1
+# Sends a 'POST' request to the passed URL and displays the body of the response
+curl -sX 'POST' -d "email=test@gmail.com&subject=I will always be here fo PLD"
