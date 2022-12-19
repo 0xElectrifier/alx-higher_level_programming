126e2f5e190935264879be587530a88fcb580956
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2022-12-19 08:02:18 +0100

    Post email and prints the response text

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-""" post email """
+"""Script that takes in a URL and an email address, sends a POST request
+to the passed URL with the email as a parameter, and finally displays
+the body of the response.
+"""
 import requests
-import sys
+from sys import argv
 
 
-if __name__ == "__main__":
-    email = {'email': sys.argv[2]}
-    r = requests.post(sys.argv[1], data=email)
-    print(r.text)
+if __name__ == '__main__':
+    url = argv[1]
+    email = argv[2]
+    value = {'email': email}
+    response = requests.post(url, value)
+
+    print(response.text)
