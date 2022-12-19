4ac266f76085b19b33c6f97a0ea78421a721aedc
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2022-12-19 23:50:34 +0100

    Forgot to push modification

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,18 +4,15 @@
 from urllib import request, parse
 
 
-req = request.Request('https://alx-intranet.hbtn.io/status')
-with request.urlopen(req) as res:
-    res_read = res.read()
-    response = [type(res_read),
-                     res_read,
-                     res.msg]
-    print("Body response:\n\
-    - type: {}\n\
-    - content: {}\n\
-    - utf8 content: {}".format(response[0],
-                               response[1],
-                               response[2]))
-
-
-#        for item in response.values()
+if __name__ == '__main__':
+    url = 'https://alx-intranet.hbtn.io/status'
+    req = request.Request(url)
+    with request.urlopen(req) as res:
+        res_read = res.read()
+        response = [type(res_read),
+                    res_read,
+                    res_read.decode()]
+        print("Body response:")
+        print("    - type: {}".format(response[0]))
+        print("    - content: {}".format(response[1]))
+        print("    - utf8 content: {}".format(response[2]))
