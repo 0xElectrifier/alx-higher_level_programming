8c49a949fa4a2ddbb24b196c958ff325089d5969
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2022-12-19 23:40:37 +0100

    Advanced task push origin master

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
+"""A Technical challenge by the Holberton School
+    - Please list 10 commits (from the most recent to oldest) of the repository
+      “rails” by the user “rails”
+    - You must use the GitHub API, here is the documentation
+      https://developer.github.com/v3/repos/commits/
+    - Print all commits by: `<sha>: <author name>` (one by line)
+This Python script takes 2 arguments, 'repository name' and 'owner name'
 """
-list 10 commits (from the most recent to oldest) of the repository and user
-sent in as arguments
-"""
+import requests
+from sys import argv
+
+
 if __name__ == '__main__':
-    import requests
-    from sys import argv
-    r = requests.get('https://api.github.com/repos/{}/{}/commits'
-                     .format(argv[2], argv[1]))
-    commits = r.json()
-    for commit in commits[:10]:
-        print(commit.get('sha'), end=': ')
-        print(commit.get('commit').get('author').get('name'))
+    url = 'https://api.github.com/repos/' + argv[2] + '/' + argv[1] + '/commits'
+    params = {'owner': 'rails', 'repo': 'rails', 'per_page': 10}
+    response = requests.get(url, params=params)
+    json_content = response.json()
+    for commit in json_content:
+        author_name = commit.get('commit').get('author').get('name')
+        print("{}: {}".format(commit.get('sha'), author_name))
