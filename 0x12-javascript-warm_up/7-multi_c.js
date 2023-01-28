f79994c38885baddf1a706009661d2ece9f86287
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-01-28 10:00:46 +0100

    Using a for loop

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-const count = process.argv[2];
-if (isNaN(count)) {
+const x = parseInt(process.argv[2]);
+if (isNaN(x)) {
   console.log('Missing number of occurences');
-}
-for (let i = 0; i < count; i++) {
-  console.log('C is fun');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
 }
