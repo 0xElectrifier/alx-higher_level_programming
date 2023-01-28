817dff6b9ede9a41dd7961fba89833356f60683f
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-01-28 01:26:50 +0100

    Added semicolon to script

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const arg = require('process').argv[2]
+const arg = require('process').argv[2];
 
 if (arg === undefined) {
   console.log('No argument');
 } else {
-  console.log(`${arg}`)
+  console.log(`${arg}`);
 }
