46439941f1df51f83e1f3327cc36b764892a4cc6
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-01-28 18:17:31 +0100

    Second biggest, resolved bug - [9, 9, 9, 8] returning 9

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,15 +3,25 @@
 function searchSecLargest (arr) {
   let largest = argv[2];
   let sLargest = argv[2];
+  let current;
   const argLen = argv.length;
   if (argLen < 4) sLargest = 0;
 
+  // Find the FIRST second largest integer
   for (let i = 2; i < argLen; i++) {
-    if (argv[i] > largest) {
-      sLargest = largest;
-      largest = argv[i];
-    } else if (argv[i] > sLargest && argv[i] !== largest) {
+    if (argv[i] < sLargest) {
       sLargest = argv[i];
+      break;
+    }
+  }
+
+  for (let i = 2; i < argLen; i++) {
+    current = argv[i];
+    if (current > largest) {
+      sLargest = largest;
+      largest = current;
+    } else if (current > sLargest && current !== largest) {
+      sLargest = current;
     }
   }
   return (sLargest);
