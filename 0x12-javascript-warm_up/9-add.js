3dc5ad633d13eab4a56f9f1270d316b87b3d04aa
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-01-28 10:48:43 +0100

    Addition of two numbers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,12 +2,9 @@
 const a = process.argv[2];
 const b = process.argv[3];
 function add (a, b) {
-  if (isNaN(a) || isNaN(b)) {
-    return (NaN);
-  } else {
-    a = parseInt(a);
-    b = parseInt(b);
-    return (a + b);
-  }
+  a = parseInt(a);
+  b = parseInt(b);
+  console.log(a + b);
 }
-console.log(add(a, b));
+
+add(a, b);
