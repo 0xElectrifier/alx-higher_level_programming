d97818c43431fc46e569d769f4cc09aeeeb1e05a
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-02-14 14:34:31 +0100

    Reversing a list

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,19 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  const rev = [];
-  for (let i = 0; i < list.length; i++) {
-    rev.unshift(list[i]);
+  if (!Array.isArray(list)) return;
+
+  let temp;
+  const listLen = list.length;
+  let firstHalf = 0;
+  let secondHalf = listLen - 1;
+
+  while (firstHalf < Math.floor(listLen / 2)) {
+    temp = list[firstHalf];
+    list[firstHalf] = list[secondHalf];
+    list[secondHalf] = temp;
+
+    firstHalf++;
+    secondHalf--;
   }
-  return rev;
-};
+  return (list);
+}
