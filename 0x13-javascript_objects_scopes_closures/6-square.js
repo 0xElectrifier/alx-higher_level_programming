0237a91a65bbfbc0d15eee35abd8d238d8edd091
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-02-14 12:17:29 +0100

    Fixed bug, prints 'X' at all time

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,19 +1,19 @@
 #!/usr/bin/node
-const Square = require('./5-square');
-module.exports = class Square extends Square {
+const Square5 = require('./5-square');
+module.exports = class Square extends Square5 {
   constructor (size) {
     super(size);
   }
 
-  charPrint(c) {
+  charPrint (c) {
     if (c === undefined) c = 'X';
 
-    width = self.width;
-    height = self.height;
+    const width = this.width;
+    const height = this.height;
     if (width && height) {
       for (let i = 0; i < height; i++) {
         for (let j = 0; j < width; j++) {
-          process.stdout.write('X');
+          process.stdout.write(c);
         }
         process.stdout.write('\n');
       }
