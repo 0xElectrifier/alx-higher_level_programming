02e11a56e0ed29ed9de85332a2ccf6f6f5e9cde9
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-02-15 15:52:31 +0100

    Script that reads a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-
 const fs = require('fs');
 const file = process.argv[2];
+if (!file) process.exit(); // Exit script if no argument was passed
 
-fs.readFile(file, 'utf-8', function (err, data) {
-  if (err) {
-    console.log(err);
+fs.readFile(file, 'utf8', (error, data) => {
+  if (error) {
+    console.log(error);
   } else {
     console.log(data);
   }
