43e769f949a068555c3ab247cc76834641d46774
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-02-15 17:14:56 +0100

    Fetching the status code of a request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,12 +1,6 @@
 #!/usr/bin/node
-
 const request = require('request');
-const URL = process.argv[2];
-
-request(URL, function (err, response) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('code: ' + response.statusCode);
-  }
+const url = process.argv[2];
+request(url, function (error, response) {
+  console.log(`code: ${response.statusCode}`);
 });
