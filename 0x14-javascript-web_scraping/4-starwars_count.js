4b3bdfa53ad5947d87415c4d388faf6b96660fe2
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-02-17 01:05:56 +0100

    Script that prints the number of movies where a CHARACTER is present

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,24 +1,23 @@
 #!/usr/bin/node
-
 const request = require('request');
 const url = process.argv[2];
+const character = 'https://swapi-api.alx-tools.com/api/people/18/';
+let id = 0;
+let wedgeAntillesOccur = 0;
 
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    const films = JSON.parse(body).results;
-    let count = 0;
-    for (const filmIndex in films) {
-      const filmChars = films[filmIndex].characters;
-      for (const charIndex in filmChars) {
-        if (filmChars[charIndex].includes('18')) {
-          count++;
-        }
-      }
-    }
-    console.log(count);
-  } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
+const fetchFilms = function (error, response, body) {
+  if (error) {
+    console.log(error);
+    process.exit();
   }
-});
+  if (response.statusCode === 200) {
+    const responseJSON = JSON.parse(body);
+    if (responseJSON.characters.includes(character)) {
+      wedgeAntillesOccur++;
+    }
+    request(url + ++id, fetchFilms);
+  } else {
+    console.log(wedgeAntillesOccur);
+  }
+};
+request(url + ++id, fetchFilms);
