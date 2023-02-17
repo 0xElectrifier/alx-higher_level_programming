da57219d769b87d34972a58d4340b39ab590cd26
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-02-17 04:38:50 +0100

    Starwars characters

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,14 +1,28 @@
 #!/usr/bin/node
-
 const request = require('request');
-const starWarsUri = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
+const movieID = process.argv[2];
+const movieURL = 'https://swapi-api.alx-tools.com/api/films/' + movieID;
+function fetchName (url) {
+  request(url, (error, response, body) => {
+    if (error) {
+      console.log(error);
+      process.exit();
+    }
+    if (response.statusCode === 200) {
+      console.log(JSON.parse(body).name);
+    }
+  });
+}
 
-request(starWarsUri, function (_err, _res, body) {
-  const characters = JSON.parse(body).characters;
-
-  for (let i = 0; i < characters.length; ++i) {
-    request(characters[i], function (_cErr, _cRes, cBody) {
-      console.log(JSON.parse(cBody).name);
-    });
+request(movieURL, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    process.exit();
+  }
+  if (response.statusCode === 200) {
+    const characters = JSON.parse(body).characters;
+    for (const charIndex in characters) {
+      fetchName(characters[charIndex]);
+    }
   }
 });
