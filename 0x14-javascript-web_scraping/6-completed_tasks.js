d9d9155caff566d21e54d89733d839165bc924d2
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-02-17 03:01:17 +0100

    Completed tasks

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,26 +1,53 @@
 #!/usr/bin/node
-
 const request = require('request');
-const url = process.argv[2];
+const url = 'https://jsonplaceholder.typicode.com/todos';
 
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    const completed = {};
-    const tasks = JSON.parse(body);
-    for (const i in tasks) {
-      const task = tasks[i];
-      if (task.completed === true) {
-        if (completed[task.userId] === undefined) {
-          completed[task.userId] = 1;
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+    process.exit();
+  }
+  var tasksOccur = {};
+  if (response.statusCode === 200) {
+    const todos = JSON.parse(body);
+    for (const taskIndex in todos) {
+      const currentTodo = todos[taskIndex];
+      const userId = currentTodo.userId;
+      // Skip uncompleted tasks
+      if (currentTodo.completed === false) continue;
+
+      // if (!Object.prototype.hasOwnProperty(tasksOccur, userId)) {
+      if (!(userId in tasksOccur)) {
+        tasksOccur[userId] = 1;
+      } else {
+        tasksOccur[userId] += 1;
+      }
+    }
+  }
+  console.log(tasksOccur);
+});
+
+/*
+let asyncFunc = async () => {
+  request(url, function (error, response, body) {
+    if (error) {
+      console.log(error);
+      process.exit();
+    }
+    if (response.statusCode === 200) {
+      const todos = JSON.parse(body);
+      const tasksOccur = {};
+      for (const taskIndex in todos) {
+        const userId = todos[taskIndex].userId;
+        if (!Object.prototype.hasOwnProperty(tasksOccur, userId)) {
+          tasksOccur[userId] = 1;
         } else {
-          completed[task.userId]++;
+          tasksOccur[userId] += 1;
         }
       }
     }
-    console.log(completed);
-  } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
-  }
-});
+  });
+  await console.log(tasksOccur);
+}
+asyncFunc();
+*/
